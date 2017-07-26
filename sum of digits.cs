dab1550ecd54ad0e74bdd2a49b5e5b7d24eae2f7
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-26 13:36:36 +0530

    sum of first 100 numbers

---
+++ sum of digits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,sum=0,r;
+            Console.WriteLine("Enter the number:");
+            num=int.Parse(Console.ReadLine());
+            while(num!=0)
+            {
+                r=num%10;
+                num=num/10;
+                sum=sum+r;
+
+            }
+            Console.WriteLine("The sum of digits " +sum);
+            Console.ReadLine();
+        }
+    }
+}
