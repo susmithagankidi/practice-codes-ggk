da959909627199dd78a664219d611c553dbcb30a
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-26 13:34:08 +0530

    sum of first 100 numbers

---
+++ sum of first 100 numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sumofnatural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,sum=0;
+            for (i=0;i<=100;i++)
+            {
+                sum=sum+i;
+            }
+             Console.WriteLine("Sum of first 100 numbers:{0}",sum);
+                
+                
+                
+        }
+    }
+}
