b050c72572778b61cdd11483fee6905283f2fb0f
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-26 13:33:37 +0530

    to string method

---
+++ toString()method.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace tostringmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           money cash1 = new money();
+           cash1.Amount=40M;
+           Console.WriteLine("cash1.ToString() returns:" +cash1.ToString());
+           Console.ReadLine();
+        }
+    }
+        public class money
+        {
+            private decimal amount;
+            public decimal Amount
+            {
+                get
+                {
+                    return amount;
+                }
+                set
+                {
+                    amount = value;
+                }
+            }
+        
+        public override string ToString()
+            {
+                return "$" +Amount.ToString();
+            }
+        
+        }
+}
+    
+
+
